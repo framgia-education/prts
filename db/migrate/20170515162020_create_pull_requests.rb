5474db1819906668af521b15ce449f084a5a8ab2
HungNH <hungnh103@gmail.com>
2017-05-16 23:18:09 +0700

    init model

---
+++ db/migrate/20170515162020_create_pull_requests.rb
@@ -0,0 +1,12 @@
+class CreatePullRequests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pull_requests do |t|
+      t.string :url
+      t.integer :status
+      t.references :user, foreign_key: true
+      t.integer :reviewer_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170515162020) do
+
+  create_table "pull_requests", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "status"
+    t.integer  "user_id"
+    t.integer  "reviewer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_pull_requests_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.integer  "stage"
+    t.boolean  "is_admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+end
