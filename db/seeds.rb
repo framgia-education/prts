c59e85c11af91002fd6cab522cd3c3e5a62609c6
HungNH <hungnh103@gmail.com>
2017-05-18 00:57:28 +0700

    deploy heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,32 @@
 User.create! full_name: "Nguyễn Huy Hùng", first_name: "Hùng",
-  email: "nguyen.huy.hungc@framgia.com", password: "123456", password_confirmation: "123456", is_admin: true
+  email: "nguyen.huy.hungc@framgia.com", password: "nguyenhuyhung01031993",
+  password_confirmation: "nguyenhuyhung01031993", is_admin: true
 
 User.create! full_name: "Nguyễn Thị Hương", first_name: "Hưong",
-  email: "nguyen.thi.huong@framgia.com", password: "123456", password_confirmation: "123456", is_admin: true
+  email: "nguyen.thi.huong@framgia.com", password: "nguyenthihuong08091994",
+  password_confirmation: "nguyenthihuong08091994", is_admin: true
 
-User.create! full_name: "Lê Tấn Thành", first_name: "Thành",
-  email: "le.tan.thanh@framgia.com", password: "123456", password_confirmation: "123456"
+User.create! full_name: "Nguyễn Bỉnh Diệu", first_name: "Diệu",
+  email: "nguyen.binh.dieu@framgia.com", password: "nguyenbinhdieu16011990",
+  password_confirmation: "nguyenbinhdieu16011990", is_admin: true
+
+User.create! full_name: "Trần Xuân Thắng", first_name: "Thắng",
+  email: "tran.xuan.thang@framgia.com", password: "tranxuanthang03091988",
+  password_confirmation: "tranxuanthang03091988", is_admin: true
+
+User.create! full_name: "Hồ Văn Tuấn", first_name: "Tuấn",
+  email: "ho.van.tuan@framgia.com", password: "hovantuan29021992",
+  password_confirmation: "hovantuan29021992", is_admin: true
+
+
+User.create! full_name: "Chu Anh Tuấn", first_name: "Tuấn",
+  email: "chu.anh.tuan@framgia.com", password: "123456", password_confirmation: "123456"
+
+User.create! full_name: "Vũ Xuân Dũng", first_name: "Dũng",
+  email: "vu.xuan.dung@framgia.com", password: "123456", password_confirmation: "123456"
+
+User.create! full_name: "Nguyễn Xuân Sơn", first_name: "Sơn",
+  email: "nguyen.xuan.son@framgia.com", password: "123456", password_confirmation: "123456"
+
+User.create! full_name: "Nguyễn Ngọc Tuấn", first_name: "Tuấn",
+  email: "nguyen.ngoc.tuan@framgia.com", password: "123456", password_confirmation: "123456"
