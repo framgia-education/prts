d84c2032d242861249bf3775dd127c3301440e83
HungNH <hungnh103@gmail.com>
2017-06-01 21:50:46 +0700

    remove keys

--- config/initializers/chatwork.rb
+++ config/initializers/chatwork.rb
@@ -1,3 +1,2 @@
-# ChatWork.api_key = ENV["CHATWORK_API_KEY"]
-ChatWork.api_key = "acd261abf1a91b2218bdfca318864c29"
+ChatWork.api_key = ENV["CHATWORK_API_KEY"]
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :hr_system, "c550483e49706ba821bccc6bac3f3b1e", "3b438b7cd973829cfa5233396968267c6c7aef298374561e6d4b4dc26c510b5c"
-  # provider :hr_system, ENV["HR_APP_ID"], ENV["HR_APP_SECRET"]
+  provider :hr_system, ENV["HR_APP_ID"], ENV["HR_APP_SECRET"]
 end
