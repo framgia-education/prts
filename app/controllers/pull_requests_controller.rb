8ff85e2a3e303c0f896cd10025361353b604c615
HungNH <hungnh103@gmail.com>
2017-07-03 20:23:52 +0700

    display pull requests by office

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,6 +1,7 @@
 class PullRequestsController < ApplicationController
   def index
-    @support = Supports::PullRequestSupport.new current_user
+    load_office_current_user unless params[:office_id]
+    @support = Supports::PullRequestSupport.new current_user, params[:office_id]
     @support_user = Supports::UserSupport.new
     @pull_requests = current_user.pull_requests.order(updated_at: :desc)
       .page params[:page]
