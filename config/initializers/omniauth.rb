385a55ff5ffd3641732a96a2cf6d6c79528d33a6
Nguyen Huy Hung <hungnh103@gmail.com>
2017-12-04 20:05:17 +0700

    Signin with TMS system

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :hr_system, ENV["HR_APP_ID"], ENV["HR_APP_SECRET"]
+  provider :framgia, ENV["TMS_APP_ID"], ENV["TMS_APP_SECRET"],
+    client_options: {
+      site: "https://education.framgia.vn/",
+      authorize_url: "https://education.framgia.vn/authorize",
+      token_url: "https://education.framgia.vn/auth/access_token"
+    }
 end
